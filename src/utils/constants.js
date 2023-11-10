012077561728773aad9b5ef05a78117f5960ec36
Yulia Korolyova <yukorolyova1@yandex.ru>
2023-11-11 02:58:46 +0300

    react диплома1.2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const MAIN_API_URL = "http://api.yulia.students.nomoredomainsrocks.ru";
+export const MAIN_API_URL = "https://api.yulia.students.nomoredomainsrocks.ru";
 export const MOVIES_API_URL = "https://api.nomoreparties.co";
